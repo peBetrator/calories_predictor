6b4e4622f74afddeb588ed73663764df9e85d74d
Nickita Mironov <nickita.mironov@deeplace.md>
2025-05-23 22:18:52 +0300

    Add environment variables to .env.dev, update .gitignore, and configure settings for the main app

---
+++ web_app/main/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import Group
+
+from unfold.admin import ModelAdmin
+from unfold.forms import UserCreationForm, UserChangeForm
+
+
+User = get_user_model()
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
